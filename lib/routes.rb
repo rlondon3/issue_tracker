3eeeec80383cf9b33d11bb5f5293d5caac3040af
rlondon3 <codesmithr@outlook.com>
2025-01-08 22:25:47 -0800

    feat: add cors

--- lib/routes.rb
+++ lib/routes.rb
@@ -5,6 +5,22 @@ require_relative 'issue_tracker'
 
 
 class Routes < Sinatra::Base
+    configure do
+        enable :cross_origin
+    end
+
+    before do
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    end
+
+    options "*" do
+        response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+        200
+    end
+    
     db = Database.new
     issue_tracker = Issue_Tracker.new(db)
 
